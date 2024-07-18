6ea8984d8158937a93576d12cfa32440567dc98a
mtgreenaway <mark.greenaway@gmail.com>
2024-07-18 17:55:28 +0100

    new bibliography

--- _bibliography/jekyll_orcid.rb
+++ _bibliography/jekyll_orcid.rb
@@ -1,52 +1,77 @@
 require 'httparty'
 require 'json'
 
-# Function to fetch and save the bibliography from an ORCID profile
-def fetch_and_save_bibliography(orcid_id, file_name)
+# Function to fetch and save the bibliography from multiple ORCID profiles
+def fetch_and_save_bibliography(orcid_person_pairs, file_name)
   base_url = "https://pub.orcid.org/v3.0"
   headers = {
     "Accept" => "application/json"
   }
 
-  # Fetch the works from the ORCID profile
-  response = HTTParty.get("#{base_url}/#{orcid_id}/works", headers: headers)
+  File.open(file_name, 'w') do |file|
+    orcid_person_pairs.each do |orcid_id, person_id|
+      # Fetch the works from the ORCID profile
+      response = HTTParty.get("#{base_url}/#{orcid_id}/works", headers: headers)
 
-  if response.success?
-    works = JSON.parse(response.body)['group']
+      if response.success?
+        works = JSON.parse(response.body)['group']
 
-    File.open(file_name, 'w') do |file|
-      works.each_with_index do |work, index|
-        summary = work['work-summary'].first
-        title = summary['title']['title']['value']
-        journal = summary['journal-title'] ? summary['journal-title']['value'] : "Unknown Journal"
-        year = summary['publication-date'] ? summary['publication-date']['year']['value'] : "Unknown Year"
-        ext_id = summary['external-ids']['external-id'].find { |id| id['external-id-type'] == 'doi' } rescue nil
-        doi = ext_id ? ext_id['external-id-value'] : "Unknown DOI"
+        works.each_with_index do |work, index|
+          summary = work['work-summary'].first
+          title = summary['title']['title']['value']
+          journal = summary['journal-title'] ? summary['journal-title']['value'] : "Unknown Journal"
+          year = summary['publication-date'] ? summary['publication-date']['year']['value'] : "Unknown Year"
+          ext_id = summary['external-ids']['external-id'].find { |id| id['external-id-type'] == 'doi' } rescue nil
+          doi = ext_id ? ext_id['external-id-value'] : "Unknown DOI"
 
-        # Generate a BibTeX entry
-        bibtex_entry = <<-BIBTEX
-@article{work#{index + 1},
+          # Fetch detailed work information to get authors, volume, and page numbers
+          work_detail_response = HTTParty.get("#{base_url}/#{orcid_id}/work/#{summary['put-code']}", headers: headers)
+          authors = "Unknown Authors"
+          volume = "Unknown Volume"
+          pages = "Unknown Pages"
+
+          if work_detail_response.success?
+            work_detail = JSON.parse(work_detail_response.body)
+            contributors = work_detail.dig('contributors', 'contributor')
+            if contributors
+              authors = contributors.map { |contributor| contributor.dig('credit-name', 'value') }.join(", ")
+            end
+
+            volume = work_detail.dig('journal-issue', 'issue') || "Unknown Volume"
+            pages = work_detail.dig('biblio', 'pages') || "Unknown Pages"
+          end
+
+          # Generate a BibTeX entry
+          bibtex_entry = <<-BIBTEX
+@article{work#{person_id}#{index + 1},
+  author = {#{authors}},
   title = {#{title}},
   journal = {#{journal}},
   year = {#{year}},
+  volume = {#{volume}},
+  pages = {#{pages}},
   doi = {#{doi}},
-  html = {https://doi.org/#{doi}},
-  bibtex_show={true}
+  url = {https://doi.org/#{doi}},
+  bibtex_show = {true}
 }
-        BIBTEX
+          BIBTEX
 
-        file.write(bibtex_entry)
+          file.write(bibtex_entry)
+        end
+
+        puts "Bibliography entries for ORCID iD: #{orcid_id} saved to #{file_name}"
+      else
+        puts "Failed to fetch data for ORCID iD: #{orcid_id}"
       end
     end
-
-    puts "Bibliography saved to #{file_name}"
-  else
-    puts "Failed to fetch data for ORCID iD: #{orcid_id}"
   end
 end
 
-# Replace this with the ORCID iD you want to query
-#orcid_id = "0000-0003-3243-3794"  #MTG
-orcid_id = "0000-0001-5672-3310"  #KM
-file_name = "bibliography_km.bib"
-fetch_and_save_bibliography(orcid_id, file_name)
+# List of ORCID iDs and corresponding person IDs
+orcid_person_pairs = [
+  ["0000-0003-3243-3794", "MTG"],
+  ["0000-0001-5672-3310", "KM"]
+]
+
+file_name = "bibliography.bib"
+fetch_and_save_bibliography(orcid_person_pairs, file_name)
--- _pages/facilities.md
+++ _pages/facilities.md
@@ -13,23 +13,18 @@ We have a large variety of facilities which are available for access to outside 
 
 Recently installed £2M Strategic Equipment from EPSRC. This multi-chamber system will enable thin film deposition by RF/DC sputtering and e-beam as well as thermal scanning probe lithography [[Nanofrazor](https://heidelberg-instruments.com/product/nanofrazor/] and scanning probe microscopy (STM/AFM) all without exposing the sample to air.  
 
-    <div class="col-sm mt-3 mt-md-0">
-      <a href="">
-        {% include figure.liquid loading="eager" path="assets/img/LBORO.jpeg" title="" class="img-fluid rounded z-depth-1" %}</a>
-    </div>
+        {% include figure.liquid loading="eager" path="assets/img/LBORO.jpeg" title="" class="img-fluid rounded z-depth-1" %}
+
 
 **Ar ion miller and Microwriter maskless photolithography**
 Class 100 clean room with maskless photolithography (resolution limit 600 nm), and argon ion milling available in Department. Training is required for use, however we can also provide patterned devices on arrangement.
 
-    <div class="col-sm mt-3 mt-md-0">
-      <a href="">
-        {% include figure.liquid loading="eager" path="assets/img/Clean_room.jpeg" title="" class="img-fluid rounded z-depth-1" %}</a>
-    </div>
+
+        {% include figure.liquid loading="eager" path="assets/img/Clean_room.jpeg" title="" class="img-fluid rounded z-depth-1" %}<
+
 
 **Cryogen-free magnetometer**
 Variable magnetic field (0-9T) and temperature (300mK – 400 K) for vibrating sample magnetometry (VSM), resistivity and thermal transport (thermal conductivity and Seebeck coefficient*) measurements.
 
-    <div class="col-sm mt-3 mt-md-0">
-      <a href="">
-        {% include figure.liquid loading="eager" path="assets/img/CFMS.jpeg" title="" class="img-fluid rounded z-depth-1" %}</a>
-    </div>
+
+        {% include figure.liquid loading="eager" path="assets/img/CFMS.jpeg" title="" class="img-fluid rounded z-depth-1" %}
